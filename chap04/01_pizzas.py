53963677cd7121bdd6175191b874f4eaca484929
Josh Clements <josh@clements.net>
2024-10-04 18:30:49 -0400

    Initial commit

---
+++ chap04/01_pizzas.py
@@ -0,0 +1,14 @@
+# Page 56
+# Try It Yourself exercise 4-1
+# Use a for loop to talk about your favorite pizzas
+
+pizzaList = [
+    'meat lover\'s',
+    'supreme', 
+    'cauliflower crust'
+]
+
+for pizza in pizzaList:
+    print(f"{pizza.title()} is one of my favorites.")
+
+print("I'd eat pizza everday if I could!")
